0e3d5e04c78ee720fc8b9755d88dde972aec0225
Gaurav Burman <gauravbrmn7@gmail.com>
2021-09-22 13:16:23 -0700

    Tip Calculator with split.py
    Day 2 of 100 days of python code

--- Tip Calculator with split.py
+++ Tip Calculator with split.py
@@ -5,8 +5,9 @@ bill = input("What was the total bill? ")
 #input for Tip
 tip = input("What percentage tip would you like to give? ")
 #Input for number of people
-pax = input("How many people to split the bill? ")
+pax = input("How many people to split the bill? $")
 #per person
-bill_per_person = round((float(bill[1:]) *(1+int(tip)/100))/int(pax),2)
+bill_per_person = round((float(bill) *(1+int(tip)/100))/int(pax),2)
 
 print(f"Each person should pay ${bill_per_person
+upda
